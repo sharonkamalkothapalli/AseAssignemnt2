738f81086441361a5fce336cd1a6f0b92815c4bb
Sharon Kamal Kothapalli <sharonkamalkothapalli@gmail.com>
2024-05-31 13:08:44 -0500

    playground, debug_toolbar, store and tags apps installed

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Sharon'})
